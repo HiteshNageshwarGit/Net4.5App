826cbf0e60b2d2ce09b3e98061c8689f12fb7090
HiteshNageshwar <nageshwarhitesh@gmail.com>
2017-12-27 14:28:29 -0500

    Implemented code first approach

---
+++ Code_First_EF/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Code_First_EF.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string Mobile { get; set; }
+    }
+}
---
+++ Code_First_EF/Models/StudentContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace Code_First_EF.Models
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext() : base("StudentContext")
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
